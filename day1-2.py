29aa6f0e464dc078a884abc6a1afddba1c07be22
Mike Wilson <mike@soarcast.com>
2024-12-04 13:35:51 -0800

    Day 1 Complete!

---
+++ day1-2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+filename = 'day1-input.txt'
+rawdata = open(filename, 'r').read()
+
+list_a = []
+list_b = []
+
+result = 0
+
+# create our lists
+for line in rawdata.split('\n'):
+    list_a.append(int(line.split(' ')[0]))
+    list_b.append(int(line.split(' ')[-1]))
+
+# sort our lists
+list_a = sorted(list_a)
+list_b = sorted(list_b)
+
+for i in range(len(list_a)):
+    result += list_a[i] * list_b.count(list_a[i])
+
+print(result)
